dcee98392d4ad5ba78e91f03af4aae52407dd582
Programming Hero <programming.hero1@gmail.com>
2022-09-30 19:55:28 -0500

    children

---
+++ __stesp.js
@@ -0,0 +1,6 @@
+/* 
+1. install react router: npm i react-router-dom
+2. create a router use createBrowserRouter
+3. Add RouterProvider and pass router props
+4. Create some route inside the router
+*/
--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
+  const router = createBrowserRouter([
+    {path: '/', element: <div>This is Default page</div>},
+    { path: '/home', element: <div>HOme Page</div>},
+    {path: 'about', element: <div>This is About Page</div>}
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
